2bf92f3c3d3f242b3bd3aa1f36f421c6f8cd65ff
Accord99 <114819430+Accord99@users.noreply.github.com>
2022-12-02 13:01:18 +0300

    решили задачу 38

---
+++ Task38/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+Console.Clear();
+
+double[] FillArray(int size)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = rnd.NextDouble()*20;
+        array[i] = Math.Round(array[i], 2);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    int size = array.Length;
+    for(int i = 0; i < size - 1; i++)
+    {
+        Console.Write($"{array[i]}; ");
+    }
+    Console.WriteLine($"{array[^1]}]");
+}
+
+double FindMax(double[] array)
+{
+    double max = array[0];
+    int size = array.Length;
+    for(int i = 1; i < size; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+double FindMin(double[] array)
+{
+    double min = array[0];
+    int size = array.Length;
+    for(int i = 1; i < size; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+double DifferenceMaxMin(double[] array)
+{
+    PrintArray(array);
+    double maximum = FindMax(array);
+    double minimum = FindMin(array);
+    double result = maximum - minimum;
+    return result;
+}
+int size2 = new Random().Next(6, 15);
+double[] massive = FillArray(size2);
+double diff = DifferenceMaxMin(massive);
+Console.WriteLine($"Разница между макисмальным и минимальным элементом массива равна {diff}");
